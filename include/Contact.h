8b3db89017558d2c30f6ba359d1ed531ff6bebdb
Benjamin Caron <caron518@gmail.com>
2024-12-18 12:17:32 -0500

    Update Contact.h

--- include/Contact.h
+++ include/Contact.h
@@ -5,15 +5,27 @@
 
 class Contact {
 public:
+    // Constructors
+    Contact();
+    Contact(std::string name, std::string phoneNumber, std::string email, std::string address);
+
+    // Getters
+    std::string getName() const;
+    std::string getPhoneNumber() const;
+    std::string getEmail() const;
+    std::string getAddress() const;
+
+    // Setters
+    void setName(const std::string& name);
+    void setPhoneNumber(const std::string& phoneNumber);
+    void setEmail(const std::string& email);
+    void setAddress(const std::string& address);
+
+private:
     std::string name;
     std::string phoneNumber;
     std::string email;
     std::string address;
-
-    Contact();
-    Contact(std::string name, std::string phoneNumber, std::string email, std::string address);
-
-    // Getters and Setters
 };
 
 #endif // CONTACT_H
