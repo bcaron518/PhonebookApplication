59e69cb5695a40b52784dbf29cfeb9b0f9ef33e5
Benjamin Caron <caron518@gmail.com>
2024-12-18 12:26:50 -0500

    Update Contact.cpp

--- src/Contact.cpp
+++ src/Contact.cpp
@@ -1,6 +1,7 @@
 #include "Contact.h"
 
-Contact::Contact() {}
+// Constructors
+Contact::Contact() : name(""), phoneNumber(""), email(""), address("") {}
 
 Contact::Contact(std::string name, std::string phoneNumber, std::string email, std::string address) {
     this->name = name;
@@ -9,4 +10,36 @@ Contact::Contact(std::string name, std::string phoneNumber, std::string email, s
     this->address = address;
 }
 
-// Getters and Setters for Contact class
+// Getters
+std::string Contact::getName() const {
+    return name;
+}
+
+std::string Contact::getPhoneNumber() const {
+    return phoneNumber;
+}
+
+std::string Contact::getEmail() const {
+    return email;
+}
+
+std::string Contact::getAddress() const {
+    return address;
+}
+
+// Setters
+void Contact::setName(const std::string& newName) {
+    name = newName;
+}
+
+void Contact::setPhoneNumber(const std::string& newPhoneNumber) {
+    phoneNumber = newPhoneNumber;
+}
+
+void Contact::setEmail(const std::string& newEmail) {
+    email = newEmail;
+}
+
+void Contact::setAddress(const std::string& newAddress) {
+    address = newAddress;
+}
