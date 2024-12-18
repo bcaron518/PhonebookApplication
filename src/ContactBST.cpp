a3f5b660f6638c7eaa864227da840211c5a6b64a
Benjamin Caron <caron518@gmail.com>
2024-12-18 11:30:03 -0500

    Create ContactBST.cpp

---
+++ src/ContactBST.cpp
@@ -0,0 +1,39 @@
+#include "ContactBST.h"
+#include <iostream>
+#include <fstream>
+
+ContactBST::ContactBST() {
+    root = nullptr;
+}
+
+void ContactBST::insert(Contact contact) {
+    // Insert contact into BST
+}
+
+Contact ContactBST::searchByName(std::string name) {
+    // Search for contact by name in BST
+}
+
+Contact ContactBST::searchByPhoneNumber(std::string phoneNumber) {
+    // Search for contact by phone number in BST
+}
+
+void ContactBST::deleteContact(std::string name) {
+    // Delete contact from BST
+}
+
+void ContactBST::updateContact(Contact contact) {
+    // Update contact information
+}
+
+void ContactBST::displayContacts() {
+    // Display all contacts in BST
+}
+
+void ContactBST::saveToFile(std::string filename) {
+    // Save contacts to file
+}
+
+void ContactBST::loadFromFile(std::string filename) {
+    // Load contacts from file
+}
